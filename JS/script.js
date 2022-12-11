9ddde583087f5581274ee36e4aa2d9a6652e7986
Эрлан Камбаров <erlankambarov94@gmail.com>
2022-12-12 02:17:45 +0600

    added popUp for form-control

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,26 @@
 // DOM-ELEMENTS
+const scrollBtn = document.querySelector('.nav-btn')
+const scrollBtn2 = document.querySelector('.banner-content-mobile-btn')
 const burger = document.querySelector('.burger')
 const sideBar = document.querySelector('.adaptive-menu')
 const closeSideBar = document.querySelector('.adaptive-menu-img-block')
+const closePopUp = document.querySelector('.close2')
+const form = document.querySelector('.form')
+const overley = document.querySelector('.overley')
 // DOM-ELEMENTS-END
 
+
+scrollBtn.addEventListener('click', () => window.scrollTo({ top: 2700, behavior: 'smooth' }))
+scrollBtn2.addEventListener('click', () => window.scrollTo({ top: 2950, behavior: 'smooth' }))
 
 burger.addEventListener('click', () => sideBar.classList.toggle('active'))
 
 closeSideBar.addEventListener('click', () => sideBar.classList.remove('active'))
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  overley.classList.toggle('active')
+})
+
+closePopUp.addEventListener('click', () => overley.classList.remove('active'))
+
